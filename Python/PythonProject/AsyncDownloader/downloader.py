1045a22ee02a4c3358e10114c0e1fc51428e3b9c
Sai Saran Uppalapati <156153731+uppalapati-saisaran@users.noreply.github.com>
2025-11-17 21:58:45 -0400

    Python - Asyncdownloder - downloading file from URL.
    (01) Downloading file from URLs.
    (02)  Starting Threads for each download file from URL.

--- Python/PythonProject/AsyncDownloader/downloader.py
+++ Python/PythonProject/AsyncDownloader/downloader.py
@@ -20,4 +20,39 @@ class FileDownloader:
         if not os.path.exists(self.download_folder):
             os.makedirs(self.download_folder)
 
+    def download_file(self,url):
+        """Downloading a single file"""
+        try:
+            self.logger.info(f"Starting download: {url}")
+            response = requests(url,timeout=10)
+            response.raise_for_status() #Raise error for bad responses
+            
+            filename = os.path.join(self.download.folder,os.path.basename(url))
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+
+            self.logger.info(f"Completed download : {filename}")    
+        except Exception as e:
+            self.logger.error(f"Error downloading {url} : {e}")
+
+    def run(self):
+        """Start threaded downloads.""" 
+        threads = []
+        for url in self.urls:
+            thread=threading.Thread(target=self.download_file,args=(url,1))
+            threads.append(thread)
+            thread.start()
+        
+        if len(thread) >= config.MAX_THREADS:
+            # Wait for current batch to finish before starting new ones.
+            for t in threads:
+                t.join()
+            threads = []
+
+        #Join any remainuing threads 
+        for t in threads:
+            t.join()
+
+        self.logger.info("All downloads completed")     
+
         
